ec1bb4c21fa0f1b44e009f6c8226890b9e11cc4c
Kishore Srinivas <kishynivas10@gmail.com>
2017-06-01 11:26:12 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')),
-    url(r'^post/(?P<pk>\d+)/$','include('blog.urls')),
-    
- ] 
+]
